76f6a7a5c1e7f0fe6b4ff02a5b777594e3666829
Yuxuan Xiong <123851861+YuxuanXiong1@users.noreply.github.com>
2023-03-27 00:37:11 +0900

    this is some change
    see

--- tracker.py
+++ tracker.py
@@ -1,64 +1,72 @@
+
 from transaction import Transaction
 import sys
 
 
 # here are some helper functions ...
 
-def print_menu():
+def print_usage():
     ''' print an explanation of how to use this command '''
     print('''menu:
             0. quit 
-            1. show transactions
-            2. add transaction
-            3. delete transaction
-            4. summarize transactions by date
-            5. summarize transactions by month
-            6. summarize transactions by year
-            7. summarize transactions by category
-            8. print this menu
+            1. show transactions (enter show)
+            2. add transaction (enter add, date fomat: YYYY-MM-DD)
+            3. delete transaction (enter delete)
+            4. summarize transactions by date (enter date)
+            5. summarize transactions by month (enter month)
+            6. summarize transactions by year (enter year)
+            7. summarize transactions by category (enter category)
+            8. print this menu (enter print)
             '''
             )
 
-def print_trans(trans):
-    ''' print the transactions '''
-    if len(trans)==0:
-        print('no transaction to print')
+def print_todos(todos):
+    ''' print the todo items '''
+    if len(todos)==0:
+        print('There is no transaction exist.')
         return
     print('\n')
-    print("%-10s %-10s %-10s %-10s %-10s"%('item #','amount','category','date','description'))
-    print('-'*40)
-    for item in trans:
-        values = tuple(item.values()) 
-        print("%-10s %-10s %-10s %-10s %-10s"%values)
+    print("%-10s %-10s %-15s %-15s %-20s"%('item #','amount','cateory','date', 'description'))
+    print('-'*70)
+    for item in todos:
+        values = tuple(item.values()) #(rowid,title,desc,completed)
+        print("%-10s %-10s %-15s %-15s %-20s"%values)
 
 def process_args(arglist):
-    ''' examine args and make appropriate calls to transaction'''
     transaction = Transaction()
     if arglist==[]:
-        print_menu()
-    elif arglist[0]=="show transactions":
-        print_trans(trans = transaction.show_tran())
-    elif arglist[0]=="showcomplete":
-        print_trans(transaction.selectCompleted())
-    elif arglist[0]=='add transaction':
-        if len(arglist)!=3:
-            print_menu()
+        print_usage()
+    elif arglist[0]=="show":
+        print_todos(todos = transaction.show())    
+    elif arglist[0]=='add':
+        if len(arglist)!=5:
+            print_usage()
         else:
-            todo = {'title':arglist[1],'desc':arglist[2],'completed':0}
+            todo = {'amount':arglist[1],'category':arglist[2],'date':arglist[3],'description':arglist[4]}
             transaction.add(todo)
-    elif arglist[0]=='complete':
-        if len(arglist)!= 2:
-            print_menu()
-        else:
-            transaction.setComplete(arglist[1])
+        pass
     elif arglist[0]=='delete':
         if len(arglist)!= 2:
-            print_menu()
+            print_usage()
         else:
             transaction.delete(arglist[1])
+    elif arglist[0]=="date":
+        print_todos(todos = transaction.dateOrder())    
+    elif arglist[0]=="month":
+        print_todos(todos = transaction.monthOrder())
+    elif arglist[0]=="year":
+        print_todos(todos = transaction.yearOrder())      
+    elif arglist[0]=="category":
+        print_todos(todos = transaction.categoryOrder())    
+    elif arglist[0] == "print":
+        print_usage()
+    elif arglist[0] == "quit":
+        sys.exit(0)
+        sys.exit(1)
+
     else:
         print(arglist,"is not implemented")
-        print_menu()
+        print_usage()
 
 
 def toplevel():
@@ -66,21 +74,25 @@ def toplevel():
     if len(sys.argv)==1:
         # they didn't pass any arguments, 
         # so prompt for them in a loop
-        print_menu()
+        print_usage()
         args = []
         while args!=['']:
             args = input("command> ").split(' ')
             if args[0]=='add':
                 # join everyting after the name as a string
-                args = ['add',args[1]," ".join(args[2:])]
+                i = 5
+                while i < len(args):
+                    args[4] = args[4] + ' ' + args[i]
+                    i = i + 1
+                args = ['add',args[1],args[2],args[3],args[4]]
             process_args(args)
-            print('-'*40+'\n'*3)
+            print('-'*70+'\n'*3)
     else:
         # read the args and process them
         args = sys.argv[1:]
         process_args(args)
-        print('-'*40+'\n'*3)
+        print('-'*70+'\n'*3)
 
-    
+
 
 toplevel()
--- transaction.py
+++ transaction.py
@@ -1,45 +1,59 @@
+'''
+todolist.py is an Object Relational Mapping to the todolist database
+The ORM will work map SQL rows with the schema
+    (rowid,title,desc,completed)
+to Python Dictionaries as follows:
+(5,'commute','drive to work',false) <-->
+{rowid:5,title:'commute',desc:'drive to work',completed:false)
+In place of SQL queries, we will have method calls.
+This app will store the data in a SQLite database ~/todo.db
+'''
 import sqlite3
 import os
 
 def toDict(t):
     ''' t is a tuple (rowid, amount, category, date, description)'''
     print('t='+str(t))
-    tran = {'rowid':t[0], 'amount':t[1], 'category':t[2], 'date':t[3], 'description' :t[4]}
-    return tran
+    todo = {'rowid':t[0], 'amount':t[1], 'category':t[2], 'date':t[3], 'description':t[4]}
+    return todo
 
 class Transaction():
     def __init__(self):
-        self.runQuery('''CREATE TABLE IF NOT EXISTS
-                    (amount int, category text, date text, descripition text)''',())
-    
-    def show_tran(self):
-        ''' return all of the uncompleted tasks as a list of dicts.'''
-        return self.runQuery("SELECT * from tracker",())
+        self.runQuery('''CREATE TABLE IF NOT EXISTS todo
+                    (amount int, category text, date text, description text)''',())
 
-    def selectAll(self):
+    def show(self):
         ''' return all of the tasks as a list of dicts.'''
-        return self.runQuery("SELECT rowid,* from todo",())
-
-    def selectCompleted(self):
-        ''' return all of the completed tasks as a list of dicts.'''
-        return self.runQuery("SELECT rowid,* from todo where completed=1",())
-
+        return self.runQuery("SELECT rowid,* FROM todo",())
+    
     def add(self,item):
         ''' create a todo item and add it to the todo table '''
-        return self.runQuery("INSERT INTO tracker VALUES(?,?,?,?)",(item['amount'],item['category'],item['date'],item['description']))
+        return self.runQuery("INSERT INTO todo VALUES(?,?,?,?)",(item['amount'],item['category'],item['date'],item['description']))
 
     def delete(self,rowid):
         ''' delete a todo item '''
         return self.runQuery("DELETE FROM todo WHERE rowid=(?)",(rowid,))
+    
+    def dateOrder(self):
+        ''' return all of the tasks as a list of dicts.'''
+        return self.runQuery("SELECT rowid,* FROM todo ORDER BY SUBSTRING(date,9,10)",())
 
-    def setComplete(self,rowid):
-        ''' mark a todo item as completed '''
-        return self.runQuery("UPDATE todo SET completed=1 WHERE rowid=(?)",(rowid,))
+    def monthOrder(self):
+        ''' return all of the tasks as a list of dicts.'''
+        return self.runQuery("SELECT rowid,* FROM todo ORDER BY SUBSTRING(date, 6, 7)",())    
+    
+    def yearOrder(self):
+        ''' return all of the tasks as a list of dicts.'''
+        return self.runQuery("SELECT rowid,* FROM todo ORDER BY SUBSTRING(date, 1, 4)",())
+    
+    def categoryOrder(self):
+        ''' return all of the tasks as a list of dicts.'''
+        return self.runQuery("SELECT rowid,* FROM todo ORDER BY category",())
 
     def runQuery(self,query,tuple):
         ''' return all of the uncompleted tasks as a list of dicts.'''
-        con= sqlite3.connect(os.getenv('HOME')+'/transactions.db')
-        cur = con.cursor() 
+        con= sqlite3.connect(os.getenv('HOME')+'/otodo.db')
+        cur = con.cursor()
         cur.execute(query,tuple)
         tuples = cur.fetchall()
         con.commit()
